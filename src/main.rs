dad7fe2edcdb795adc9c9da97a3f46dfbc946356
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-03-04 22:33:26 -0300

    feat: impl-draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    let content = std::fs::read_to_string(&args.path).expect("file can not be open");
+
+    for line in content.lines(){
+        if line.contains(&args.pattern){
+            println!("  -> {}", line);
+        }
+    }
+
 }
