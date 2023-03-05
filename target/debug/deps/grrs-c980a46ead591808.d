0d55020a7c71dc176cec25d679738e26ca3424c8
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-03-04 22:14:23 -0300

    🎉 first commit

---
+++ target/debug/deps/grrs-c980a46ead591808.d
@@ -0,0 +1,5 @@
+/media/jnaraujo/Files/Programming/command_line_rust_book/grrs/target/debug/deps/grrs-c980a46ead591808.rmeta: src/main.rs
+
+/media/jnaraujo/Files/Programming/command_line_rust_book/grrs/target/debug/deps/grrs-c980a46ead591808.d: src/main.rs
+
+src/main.rs:
