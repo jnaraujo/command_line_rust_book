4dae50c873e47da844230e5bb2fead6410704a7b
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-03-04 22:16:01 -0300

    🙈 add gitignore

--- target/debug/deps/grrs-21f78dc75e6d6cd5.d
+++
@@ -1,5 +0,0 @@
-/media/jnaraujo/Files/Programming/command_line_rust_book/grrs/target/debug/deps/grrs-21f78dc75e6d6cd5.rmeta: src/main.rs
-
-/media/jnaraujo/Files/Programming/command_line_rust_book/grrs/target/debug/deps/grrs-21f78dc75e6d6cd5.d: src/main.rs
-
-src/main.rs:
